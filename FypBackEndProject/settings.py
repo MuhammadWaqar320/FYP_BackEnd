2b4dc32d59d2e30d354adb2895e71ea4cf41114c
M Waqar <m.waqar.c320@gmail.com>
2021-08-29 12:24:19 -0700

    first commit

--- FypBackEndProject/settings.py
+++ FypBackEndProject/settings.py
@@ -137,7 +137,6 @@ USE_TZ = True
 CORS_ALLOWED_ORIGINS = [
     'https://pakelectronics.herokuapp.com/',
     'http://pakelectronics.herokuapp.com/',
-   ,
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
