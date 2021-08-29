08d3fd3395594e545fb7beadd86d61b2c71b5cd1
M Waqar <m.waqar.c320@gmail.com>
2021-08-29 12:34:23 -0700

    add requirements.txt

--- FypBackEndProject/settings.py
+++ FypBackEndProject/settings.py
@@ -135,8 +135,8 @@ USE_TZ = True
 
 
 CORS_ALLOWED_ORIGINS = [
-    'https://pakelectronics.herokuapp.com/',
-    'http://pakelectronics.herokuapp.com/',
+    # 'https://pakelectronics.herokuapp.com/',
+    # 'http://pakelectronics.herokuapp.com/',
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
